89c9bcf40e6c6cfce0bbc7781338f1112c4cd2e7
krishna singh <0.krishna1120@gmail.com>
2026-06-10 12:58:30 +0530

    chore: add package.json with express dependency

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const users = [{
+    id: 1,
+    username: "krishna",
+    password: "123123"
+
+} , {
+    id: 2,
+    username: "Aman",
+    password: "123123"
+}]
+const organisations = [{
+    id: 1,
+    title: "devx",
+    description: "Learnings",
+    admin: 1,
+    member: [2]
+
+} , {
+    id:2,
+    title: "ahlele",
+    description: "doing",
+    admin: 1,
+    member: []
+}]
+const boards = [{
+    id: 1,
+    title: "devs website (frontend)",
+    organizationId: 1
+}]
+const isssues = [{
+    id: 1,
+    title: "add dark mode",
+    boardId: 1,
+    state: "IN_PROGRESS"
+},{
+    id: 2,
+    title: "allow admins to create more courses",
+    boardId: 1,
+    state: "DONE"
+}]
+
+const app = express()
+app.listen(3005);
