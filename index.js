1a64038c2327669b1f60dcfb267c7131de1b5264
krishna singh <0.krishna1120@gmail.com>
2026-06-14 12:34:18 +0530

    feat: implement user signup and signin endpoints

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
 const express = require("express");
-const users = [{
+
+let USER_ID = 1
+let ORGANIZATION_ID = 1
+let BOARD_ID = 1
+let ISSUE_ID = 1
+
+const USERS = [{
     id: 1,
     username: "krishna",
     password: "123123"
 
-} , {
-    id: 2,
-    username: "Aman",
-    password: "123123"
 }]
-const organisations = [{
+const ORGANIZATIONS = [{
     id: 1,
     title: "devx",
     description: "Learnings",
     admin: 1,
     member: [2]
 
-} , {
-    id:2,
-    title: "ahlele",
-    description: "doing",
-    admin: 1,
-    member: []
 }]
-const boards = [{
+const BOARDS = [{
     id: 1,
     title: "devs website (frontend)",
     organizationId: 1
 }]
-const isssues = [{
+const ISSUES = [{
     id: 1,
     title: "add dark mode",
     boardId: 1,
     state: "IN_PROGRESS"
-},{
-    id: 2,
-    title: "allow admins to create more courses",
-    boardId: 1,
-    state: "DONE"
 }]
 
 const app = express()
 
-app.post("/organization")
+app.post("/signup",(req,req)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userExists = USERS.find(u => u.username === username);
+    if(userExists){
+        res.status(411).json({
+            message: "User witht his username already exists"
+        })
+        return;
+    }
+    USERS.PUSH({
+        username,
+        password,
+        id: USER_ID++
+    })
+})
+
+app.post("/signin" ,(req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userExists = USERS.find(u=>u.username === username && u.password === password) ;
+    if(!userExists){
+        res.status(403).json({
+            message: "incorrect credentials"
+        })
+    }
+    
+})
 
 
 
